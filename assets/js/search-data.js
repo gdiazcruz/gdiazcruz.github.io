120075a4502e40df14abebb7cff13fff8a6bdf0f
gdiazcruz <50457604+gdiazcruz@users.noreply.github.com>
2025-03-09 23:40:33 +0000

    Deploying to gh-pages from @ gdiazcruz/gdiazcruz.github.io@8aa81194224b4521bb7b03e7af523c860ccce165 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Read about what we are working on.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
