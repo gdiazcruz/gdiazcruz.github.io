8ad420b86f892dc8e24c657e5d4cb9e9cfb2eecb
gdiazcruz <50457604+gdiazcruz@users.noreply.github.com>
2025-03-10 00:13:45 +0000

    Deploying to gh-pages from @ gdiazcruz/gdiazcruz.github.io@0ea4b2abf34754cbd281eee3b365c149d44edd51 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -405,9 +405,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-streptomyces",
+          title: 'Streptomyces',
+          description: "Characterization of plant pathogenic Streptomyces species infecting potatoes in Costa Rica",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
