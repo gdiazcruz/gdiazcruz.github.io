557feb9155bee55f0689a4fb7d0a8cc46a50538b
gdiazcruz <50457604+gdiazcruz@users.noreply.github.com>
2025-03-16 03:46:36 +0000

    Deploying to gh-pages from @ gdiazcruz/gdiazcruz.github.io@2998a28e268a3086c50e5bdff5a9aecd94afd41f 🚀

--- es/assets/js/search-data.js
+++ es/assets/js/search-data.js
@@ -14,8 +14,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-acerca-de",
-    title: "Acerca de",
+    id: "nav-about",
+    title: "about",
     section: "Menú de navegación",
     handler: () => {
       window.location.href = "/es/";
